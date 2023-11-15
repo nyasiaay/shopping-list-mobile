9185bd2e51be027e78f378a7d086ba354e2d8465
nyasiaay <nyasia.aludra@ui.ac.id>
2023-11-15 15:49:21 +0700

    Tutorial 8

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,76 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/screens/menu.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
+import 'package:shopping_list/screens/list_product.dart';
 
 class LeftDrawer extends StatelessWidget {
-  const LeftDrawer({super.key});
+    const LeftDrawer({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          const DrawerHeader(
-            // TODO: Bagian drawer header
-                decoration: BoxDecoration(
-                  color: Colors.indigo,
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      'Shopping List',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
+    @override
+    Widget build(BuildContext context) {
+        return Drawer(
+            child: ListView(
+                children: [
+                    const DrawerHeader(
+                        decoration: BoxDecoration(
+                            color: Colors.indigo,
+                        ),
+                        child: Column(
+                            children: [
+                                Text(
+                                    'Shopping List',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    ),
+                                ),
+                                Padding(padding: EdgeInsets.all(10)),
+                                Text("Catat seluruh keperluan belanjamu di sini!",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.white, 
+                                        fontWeight: FontWeight.normal,
+                                    ),
+                                    // TODO: Tambahkan gaya teks dengan center alignment, font ukuran 15, warna putih, dan weight biasa
+                                ),
+                            ],
+                        ),
                     ),
-                    Padding(padding: EdgeInsets.all(10)),
-                    Text(
-                    "Catat seluruh keperluan belanjamu di sini!",
-                    style: TextStyle(
-                      fontSize: 15,      // Ukuran font 15
-                      color: Colors.white,  // Warna teks putih
-                      fontWeight: FontWeight.normal,  // Weight biasa
+                    ListTile(
+                        leading: const Icon(Icons.home_outlined),
+                        title: const Text('Halaman Utama'),
+                        // Bagian redirection ke MyHomePage
+                        onTap: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MyHomePage(),
+                                )
+                            );
+                        },
                     ),
-                    textAlign: TextAlign.center, // Center alignment
-                  )
-                  ],
-                ),
-              ),
-          
-          // TODO: Bagian routing
-          ListTile(
-            leading: const Icon(Icons.home_outlined),
-            title: const Text('Halaman Utama'),
-            // Bagian redirection ke MyHomePage
-            onTap: () {
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MyHomePage(),
-                  ));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.add_shopping_cart),
-            title: const Text('Tambah Produk'),
-            // Bagian redirection ke ShopFormPage
-            onTap: () {
-              /*
-              TODO: Buatlah routing ke ShopFormPage di sini,
-              setelah halaman ShopFormPage sudah dibuat.
-              */
-               Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ShopFormPage(),
-                  ));
-            },
-          ),
-        ],
-      ),
-    );
-  }
+                    ListTile(
+                        leading: const Icon(Icons.add_shopping_cart),
+                        title: const Text('Tambah Produk'),
+                        // Bagian redirection ke ShopFormPage
+                        onTap: () {
+                            /*
+                            TODO: Buatlah routing ke ShopFormPage di sini,
+                            setelah halaman ShopFormPage sudah dibuat.
+                            */
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ShopFormPage(),
+                                )
+                            );
+                        },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.shopping_basket),
+                      title: const Text('Daftar Produk'),
+                      onTap: () {
+                          // Route menu ke halaman produk
+                          Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const ProductPage()),
+                          );
+                      },
+                  ),
+                ],
+            ),
+        );
+    }
 }
